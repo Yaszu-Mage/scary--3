4872987622f4e38652e615c659f0e1fa5542b7f2
Yaszu <76853160+Yaszu-Mage@users.noreply.github.com>
2024-10-12 17:59:41 -0700

    Initial commit

---
+++ assets/scrips/global.gd
@@ -0,0 +1,17 @@
+extends Node
+var mobile = false
+var controller = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("quit"):
+		get_tree().quit()
